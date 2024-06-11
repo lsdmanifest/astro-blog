6f0ac7b23cf4fe56a334ab49a7beb0c43a4cefe9
lsdmanifest <lsd.manifest@gmail.com>
2024-06-11 13:51:51 +0300

    Finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astrofuckingblog.netlify.app"
+  site: "https://astrofuckingblog.netlify.app",
+  integrations: [preact()]
 });
